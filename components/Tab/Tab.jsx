dbf3b22c51e05ffb11f98750ac15a2027e4cb892
tiance <clearfix@sina.com>
2016-03-30 17:10:43 +0800

    'tabSfixed'

--- components/Tab/Tab.jsx
+++ components/Tab/Tab.jsx
@@ -1,28 +1,47 @@
-import React, {Component, PropTypes} from 'react';
-import classnames from 'classnames';
-import Loading from '../Loading';
 
-class Tab extends Component{
+import React, { Component, PropTypes } from 'react';
+import classnames from 'classnames';
+
+class Tab extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: props.selected || props.defaultSelected,
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if ('selected' in nextProps) {
+      this.setState({
+        selected: !!nextProps.selected
+      });
+    }
+  }
+
   render () {
     const props = this.props;
-    const {isActive, className, children, ...others} = props;
+    const { children, selected, ...others } = props;
+
     const cls = classnames({
-      'ui-tab-active' : ('active' in props || isActive),
-      [className]: !!className
-    });
+      'ui-tab-bd-item': true,
+      'ui-tab-bd-item-active': this.state.selected
+    })
+
     return (<div {...others} className={cls}>
       {children}
     </div>);
   }
 }
 
-
 Tab.propTypes = {
-
+  defaultChecked: PropTypes.bool,
+  isDisabled    : PropTypes.bool,
 };
 
-Tab.defaultProps ={
-
+Tab.defaultProps = {
+  defaultChecked: false,
+  isDisabled    : false,
 };
 
 export default Tab;
