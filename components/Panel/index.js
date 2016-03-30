149ab27314b1789099184234fd8d290483ef0b6b
JeromeLin <16908091@qq.com>
2016-03-30 23:58:54 +0800

    init

--- components/Panel/index.js
+++
@@ -1,9 +0,0 @@
-
-import Panel from './Panel';
-import PanelHeader from './PanelHeader';
-import PanelBody from './PanelBody';
-
-Panel.Header = PanelHeader;
-Panel.Body = PanelBody;
-
-export default Panel;
--- components/Select/index.js
+++
@@ -1,7 +0,0 @@
-
-import Select from './Select';
-import SelectOption from './Option';
-
-Select.Option = Option;
-
-export default Select;
