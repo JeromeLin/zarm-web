f78d23f8661acdb0af86f26a67f4d44f3b111e89
JeromeLin <16908091@qq.com>
2016-05-17 00:05:36 +0800

    webpack config

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,49 +1,94 @@
 var webpack = require('webpack');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var path = require('path');
-var pkg = require('./package');
 
 module.exports = {
+
+  output: {
+    path: path.join(process.cwd(), 'assets'),
+    filename: 'js/[name].min.js',
+    chunkFilename: 'js/[name].[chunkhash:8].min.js',
+    publicPath: './'
+  },
 
   module: {
     loaders: [
       { 
-        test: /\.(js|jsx)$/, 
-        loader: 'babel',
-        exclude: /node_modules/
+        test: /\.scss$/, 
+        loader: ExtractTextPlugin.extract("style-loader", "css?sourceMap&-minimize!autoprefixer!sass?sourceMap")
       },
       { 
-        test: /\.scss$/, 
-        loader: ExtractTextPlugin.extract(
-          "style-loader",
-          "css-loader?sourceMap&-minimize!sass-loader"
-        )
-      },
-      {
         test: /\.css$/, 
-        loader: ExtractTextPlugin.extract(
-          "style-loader",
-          "css-loader?sourceMap&-minimize"
-        )
+        loader: ExtractTextPlugin.extract("style-loader", "css?sourceMap&-minimize!autoprefixer")
       },
       {
-        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
-        loader: 'url-loader?limit=8192'
+        test: /\.(png|jpg|jpeg|gif)$/,
+        loader: 'url-loader?limit=8192&name=images/[hash:8].[name].[ext]'
       },
       {
-        test: /\.(eot|ttf|wav|mp3)$/,
-        loader: 'file-loader'
+        test: /\.(woff|woff2|ttf|eot|svg)$/,
+        loader: 'file-loader?name=fonts/[name].[ext]'
+      },
+      {
+        test: /\.(html)$/,
+        loader: 'file-loader?name=[name].[ext]'
       }
     ]
   },
 
   plugins: [
-    new ExtractTextPlugin('[name].min.css')
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.NoErrorsPlugin(),
   ],
+
   
   resolve: {
     extensions: ['', '.js', '.jsx']
-  },
+  }
 
-  devtool: 'source-map'
 };
+
+// module.exports = {
+
+//   module: {
+//     loaders: [
+//       { 
+//         test: /\.(js|jsx)$/, 
+//         loader: 'babel',
+//         exclude: /node_modules/
+//       },
+//       { 
+//         test: /\.scss$/, 
+//         loader: ExtractTextPlugin.extract(
+//           "style-loader",
+//           "css-loader?sourceMap&-minimize!sass-loader"
+//         )
+//       },
+//       {
+//         test: /\.css$/, 
+//         loader: ExtractTextPlugin.extract(
+//           "style-loader",
+//           "css-loader?sourceMap&-minimize"
+//         )
+//       },
+//       {
+//         test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
+//         loader: 'url-loader?limit=8192'
+//       },
+//       {
+//         test: /\.(eot|ttf|wav|mp3)$/,
+//         loader: 'file-loader'
+//       }
+//     ]
+//   },
+
+//   plugins: [
+//     new ExtractTextPlugin('[name].min.css')
+//   ],
+  
+//   resolve: {
+//     extensions: ['', '.js', '.jsx']
+//   },
+
+//   devtool: 'source-map'
+// };
