faf04d71cebb782911d7a01f84cc098a1e2a39f8
chrisHchen <chenchris1986@163.com>
2016-06-16 01:47:22 +0800

    modify slider

--- components/Slider/index.jsx
+++ components/Slider/index.jsx
@@ -1,6 +1,8 @@
 import React, { Component, PropTypes } from 'react';
+import classnames from 'classnames';
 import domUtil from '../utils/dom';
 export default class Slider extends Component{
+
 	constructor(props){
 		super(props)
     this.state = {
@@ -10,10 +12,12 @@ export default class Slider extends Component{
     this.onHandleDown = this.onHandleDown.bind(this)
     this.onHandleMove = this.onHandleMove.bind(this)
     this.onHandleUp = this.onHandleUp.bind(this)
+    this.clickhandler = this.clickhandler.bind(this)
 
     this.draggingPayload  = {
     	isDragging: false,
-    	prevX : 0
+    	prevX : 0,
+    	isThreshhold : false // 是否到达阈值
     }
 	}
 
@@ -27,7 +31,7 @@ export default class Slider extends Component{
 			this.setState({
 				currentValue:initValue
 			})
-		},0)
+		}, 0)
 
 		this.offsetLeft = domUtil.getLeft(this.refs.sliderBody)
 	}
@@ -37,6 +41,8 @@ export default class Slider extends Component{
 		e.preventDefault()
 		document.body.addEventListener('mousemove', this.onHandleMove , false)
 		document.body.addEventListener('mouseup', this.onHandleUp , false)
+		// 移除初始化时的transition效果，否则会影响slider的推动
+		this.removeTransition = true 
 
 		this.draggingPayload.isDragging  = true
 		this.draggingPayload.prevX = e.clientX
@@ -48,17 +54,35 @@ export default class Slider extends Component{
 		if(this.draggingPayload.isDragging){
 			
 			const {min, max, step, styleWidth, getValue} = this.props
-			const mouseLeft = e.clientX - this.offsetLeft
-			const percent = mouseLeft / (styleWidth || 200)
-			const realPercent = percent > 1 ? 1 : 
-								(percent < 0 ? 0 : percent)
-			if( percent < 1.05 && percent > -0.05 ){
-				const value = Math.floor(realPercent * (max - min)) + min
-				if (value % step == 0){
+			const mouseMovedDist = e.clientX - this.draggingPayload.prevX
+			const percent = mouseMovedDist / (styleWidth || 200)
+			const value = percent * ( max - min )
+			if( Math.abs(value) >= step ){
+				const closestStepToValue = Math.round(value/step) * step
+				const newValue = this.state.currentValue + closestStepToValue
+				//console.log(newValue)
+
+				if( newValue <= max  && newValue >= min ){
+					getValue && getValue(this.state.currentValue + closestStepToValue)
+					this.draggingPayload.prevX = e.clientX
+					this.draggingPayload.isThreshhold = false
 					this.setState({
-						currentValue:value
+						currentValue:this.state.currentValue + closestStepToValue
 					})
-					getValue && getValue(this.state.currentValue)
+				}else if( newValue > max && !this.draggingPayload.isThreshhold){
+					getValue && getValue(max)
+					this.draggingPayload.prevX = e.clientX
+					this.draggingPayload.isThreshhold = true
+					this.setState({
+						currentValue: max
+					})
+				}else if( newValue < min && !this.draggingPayload.isThreshhold ){
+					getValue && getValue(min)
+					this.draggingPayload.prevX = e.clientX
+					this.draggingPayload.isThreshhold = true
+					this.setState({
+						currentValue: min
+					})
 				}
 			}
 		}
@@ -70,28 +94,71 @@ export default class Slider extends Component{
 		document.body.removeEventListener('mousemove', this.onHandleMove , false)
 		document.body.removeEventListener('mouseup', this.onHandleUp , false)
 
-		this.draggingPayload.prevX = e.clientX
 		this.draggingPayload.isDragging  = false
 	}
 
+	clickhandler(e){
+		e.stopPropagation()
+		e.preventDefault()
+		this.removeTransition = false
+
+		const {min, max, step, styleWidth, getValue} = this.props
+		const mouseLeft = e.clientX - this.offsetLeft
+		if( mouseLeft < 0 || mouseLeft > (styleWidth || 200) ) return
+		const percent = mouseLeft / (styleWidth || 200)
+		const value = Math.floor(percent * (max - min)) + min
+		const delta = value - this.state.currentValue
+		if ( Math.abs(delta) >= step ){
+			const closestStepToDelta = Math.round(delta/step) * step
+
+			getValue && getValue(this.state.currentValue + closestStepToDelta)
+			this.setState({
+				currentValue:this.state.currentValue + closestStepToDelta
+			})
+		}
+	}
+
 	render(){
-		const {min, max, step, defaultValue, styleWidth} = this.props
+		const {min, max, step, defaultValue, styleWidth, showTip, tipAlwayShow, customCls} = this.props
 		const percent = (this.state.currentValue - min) / (max - min) * 100 
 
-		//console.log(this.state.currentValue)
-		const styleObj = {
+		const styleWidthObj = {
 			width : (styleWidth || 200) + 'px' 
 		}
+		const tipClass = classnames({
+			'ui-slider-tip' : true,
+			'ui-slider-tip-show' : tipAlwayShow
+		})
+		const customClass = classnames({
+			'ui-slider-wraper' : true,
+			[customCls] : customCls
+		})
+
+		const tip = (<em className={tipClass}>{this.state.currentValue}</em>)
+		const transitionArray = ['WebkitTransition', 'MozTransition', 'msTransition', 'OTransition', 'Transition']
+		const styleObj = {
+			handle : { left: `${percent}%` },
+			back : { width: `${percent}%` },
+		}
+
+		if(!this.removeTransition){
+			transitionArray.forEach(( transition ) => {
+				styleObj.handle[ transition ] = 'left 0.6s ease-out'
+				styleObj.back[ transition ] = 'width 0.6s ease-out'
+			})
+		}
+
 		return(
-			<div className='ui-slider-wraper'>
-				<div className='ui-slider-horizontal' style={styleObj} ref='sliderBody'>
-					<span className='ui-slider-handle' 
-								style={{left:`${percent}%`}}
-								onMouseDown = {this.onHandleDown}
-								onMouseMove = {this.onHandleMove} 
-								onMouseUp = {this.onHandleUp} >
+			<div className={customClass}>
+				<div className='ui-slider-horizontal' style = { styleWidthObj } ref='sliderBody' onClick = { this.clickhandler }>
+					<span className='ui-slider-handle' style = { styleObj.handle }
+								onMouseDown = { this.onHandleDown }
+								onMouseMove = { this.onHandleMove } 
+								onMouseUp = { this.onHandleUp } 
+								onClick = {(e) => e.stopPropagation()}>
+					{showTip && tip}
 					</span>
-					<div className='ui-slider-back' style={{width:`${percent}%`}}></div>
+					<div className='ui-slider-back' style = { styleObj.back }></div>
 				</div>
 			</div>
 		)
@@ -104,11 +171,16 @@ Slider.propTypes = {
 	step : PropTypes.number,
 	defaultValue : PropTypes.number,
 	styleWidth: PropTypes.number,
-	getValue:PropTypes.func
+	getValue : PropTypes.func,
+	showTip : PropTypes.bool,
+	tipAlwayShow : PropTypes.bool,
+	customCls : PropTypes.string
 }
 
 Slider.defaultProps = {
 	min : 0,
 	step : 1,
-	defaultValue : 0
+	defaultValue : 0,
+	showTip : true,
+	tipAlwayShow: true
 }
--- examples/pages/SliderPage.jsx
+++ examples/pages/SliderPage.jsx
@@ -3,13 +3,15 @@ import Button from '../../components/Button';
 import Slider from '../../components/Slider';
 
 import '../../styles/index.scss';
+import '../styles/pages/Slider.scss';
 
 class SliderPage extends Component{
+	
 	constructor(props){
 		super(props)
 		this.state = {
-			value1:0,
-			value2:0
+			value1:180,
+			value2:220
 		}
 		this.getValue1 = this.getValue1.bind(this)
 		this.getValue2 = this.getValue2.bind(this)
@@ -26,26 +28,34 @@ class SliderPage extends Component{
 		})
 	}
 
-	componentDidMount(){
-
-	}
-
 	render(){
+
 		return(
 			<div style={{margin:'20px 20px 0 50px'}}>
-
-				max = 200, 默认(min = 0, defaultValue = 0, step =1)
 				<SliderContainer max = {200}/>
-				min = 0, max = 300, step = 1, defaultValue = 80
+				max = 200, 默认(min = 0, defaultValue = 0, step =1)
+				
 				<SliderContainer min = {0} max = {300} step = {1} defaultValue = {80} />
-				<div>min = 40, max = 600, step = 10, defaultValue = 180</div>
-				<SliderContainer min = {40} max = {400} step = {10} defaultValue = {180} getValue = {this.getValue1}/>
+				min = 0, max = 300, step = 1, defaultValue = 80
+
+				<SliderContainer min = {40} max = {400} step = {5} defaultValue = {this.state.value1} 
+												getValue = {this.getValue1} showTip = {false}/>
 				<Button>{this.state.value1}</Button>
-			
-				<div>min = 40, max = 600, step = 1, defaultValue = 220, styleWidth = 800</div>
-				<SliderContainer min = {40} max = {600} step = {1} defaultValue = {220} 
-													getValue = {this.getValue2} styleWidth={800}/>
+				<div>配合其他component显示当前value: min = 40, max = 600, step = 5, defaultValue = 180, showTip = false</div>
+
+				<SliderContainer min = {60} max = {600} step = {1} defaultValue = {this.state.value2} 
+													getValue = {this.getValue2} styleWidth={800} tipAlwayShow = {false}/>
 				<Button>{this.state.value2}</Button>
+				<div>min = 60, max = 600, step = 1, defaultValue = 220, styleWidth = 800, tipAlwayShow = false</div>
+
+				<SliderContainer min = {0} max = {10000} step = {500} defaultValue = {0} styleWidth={300}/>
+				<div>有较大step的slider： min = 0, max = 10000, step = 500, defaultValue = 0, styleWidth = 300</div>
+
+				<SliderContainer min = {-100} max = {300} step = {1} defaultValue = {-50} />
+				带负值的slider: min = -100, max = 300, step = 1, defaultValue = 80
+
+				<SliderContainer min = {0} max = {300} step = {1} defaultValue = {80} customCls = 'mySlider'/>
+				自定义样式: min = 0, max = 300, step = 1, defaultValue = 80, customCls = 'mySlider'
 			</div>
 		)
 	}
@@ -55,7 +65,7 @@ const SliderContainer = ({
 	...params
 }) => {
 	return(
-		<div>
+		<div style = {{margin:'50px 0 10px'}}>
 		 <Slider {...params}/>
 		</div>
 	)
