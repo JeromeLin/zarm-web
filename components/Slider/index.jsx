b7ed2962ca6469f4502401362354aa3fe01c7785
chrisHchen <chenchris1986@163.com>
2016-06-18 05:14:44 +0800

    modify slider as required

--- components/Slider/index.jsx
+++ components/Slider/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import classnames from 'classnames';
 import domUtil from '../utils/dom';
+
 export default class Slider extends Component{
 
 	constructor(props){
@@ -8,7 +9,7 @@ export default class Slider extends Component{
 		const {HandleAmount} = this.props
 		let states = {}, prevXs = {}, i = 0
 		while(i < HandleAmount){
-			states = Object.assign(states,{[`currentValue${i}`] : 0})
+			states[`currentValue${i}`] = 0
 			i++
 		}
     this.state = states
@@ -49,18 +50,20 @@ export default class Slider extends Component{
 		return (e) => {
 			e.stopPropagation()
 			e.preventDefault()
+			const addBodyListener = document.body.addEventListener.bind(document.body)
+
 			this.draggingPayload.handleMove = this.onHandleMove(i)
 			this.draggingPayload.handleUp = this.onHandleUp(i)
+
 			if(!this.isTouchSuported){
-				document.body.addEventListener('mousemove', this.draggingPayload.handleMove , false)
-				document.body.addEventListener('mouseup', this.draggingPayload.handleUp , false)
+				addBodyListener('mousemove', this.draggingPayload.handleMove , false)
+				addBodyListener('mouseup', this.draggingPayload.handleUp , false)
 			}else{
-				document.body.addEventListener('touchmove', this.draggingPayload.handleMove , false)
-				document.body.addEventListener('touchend', this.draggingPayload.handleUp , false)
+				addBodyListener('touchmove', this.draggingPayload.handleMove , false)
+				addBodyListener('touchend', this.draggingPayload.handleUp , false)
 			}
 			// 移除初始化时的transition效果，否则会影响slider的推动
 			this.removeTransition = true 
-
 			this.draggingPayload.isDragging  = true
 			this.draggingPayload.prevX = this.isTouchSuported ? e.touches[0].clientX : e.clientX
 		}
@@ -71,34 +74,35 @@ export default class Slider extends Component{
 			e.stopPropagation()
 			e.preventDefault()
 			if(this.draggingPayload.isDragging){
-				
-				const {min, max, step, styleWidth, getValue} = this.props
-				const mouseMovedDist = ((this.isTouchSuported && e.touches && e.touches[0]) ? e.touches[0].clientX : e.clientX) - this.draggingPayload.prevX
+				const {min, max, step, styleWidth, getValue, isPass} = this.props
+				const mouseMovedDist = ((this.isTouchSuported && e.touches && e.touches[0]) ? 
+																e.touches[0].clientX : 
+																e.clientX) - this.draggingPayload.prevX
+
 				const percent = mouseMovedDist / (styleWidth || 200)
 				const value = percent * ( max - min )
 				if( Math.abs(value) >= step ){
 					//寻找最接近value的step倍数
 					const closestStepToValue = Math.round(value/step) * step
 					const newValue = this.state[`currentValue${i}`] + closestStepToValue
-					//console.log(newValue)
 
+					if(!isPass && this.isHandleMeet(i, newValue)) return
+
+					this.draggingPayload.prevX = (this.isTouchSuported && e.touches && e.touches[0]) ? e.touches[0].clientX : e.clientX
 					if( newValue <= max  && newValue >= min ){
-						getValue && getValue(this.state[`currentValue${i}`] + closestStepToValue, i)
-						this.draggingPayload.prevX = (this.isTouchSuported && e.touches && e.touches[0]) ? e.touches[0].clientX : e.clientX
+						getValue && getValue(newValue, i)
 						this.draggingPayload.isThreshhold = false
 						this.setState({
-							[`currentValue${i}`]:this.state[`currentValue${i}`] + closestStepToValue
+							[`currentValue${i}`]:newValue
 						})
 					}else if( newValue > max && !this.draggingPayload.isThreshhold){
 						getValue && getValue(max, i)
-						this.draggingPayload.prevX = (this.isTouchSuported && e.touches && e.touches[0]) ? e.touches[0].clientX : e.clientX
 						this.draggingPayload.isThreshhold = true
 						this.setState({
 							[`currentValue${i}`]: max
 						})
 					}else if( newValue < min && !this.draggingPayload.isThreshhold ){
 						getValue && getValue(min, i)
-						this.draggingPayload.prevX = this.isTouchSuported ? e.touches[0].clientX : e.clientX
 						this.draggingPayload.isThreshhold = true
 						this.setState({
 							[`currentValue${i}`]: min
@@ -113,30 +117,37 @@ export default class Slider extends Component{
 		return (e) => {
 			e.stopPropagation()
 			e.preventDefault()
+			const removeBodyListener = document.body.removeEventListener.bind(document.body)
+
 			if(!this.isTouchSuported){
-				document.body.removeEventListener('mousemove', this.draggingPayload.handleMove , false)
-				document.body.removeEventListener('mouseup', this.draggingPayload.handleUp, false)
+				removeBodyListener('mousemove', this.draggingPayload.handleMove , false)
+				removeBodyListener('mouseup', this.draggingPayload.handleUp, false)
 			}else{
-				document.body.removeEventListener('touchmove', this.draggingPayload.handleMove , false)
-				document.body.removeEventListener('touchend', this.draggingPayload.handleUp, false)
+				removeBodyListener('touchmove', this.draggingPayload.handleMove , false)
+				removeBodyListener('touchend', this.draggingPayload.handleUp, false)
 			}
-			this.draggingPayload.isDragging  = false
+			//fix ‘click event triggered after mouseup’ bug
+			setTimeout(()=>{
+				this.draggingPayload.isDragging  = false
+			}, 0)
 		}
 	}
 
 	clickhandler(e){
 		e.stopPropagation()
 		e.preventDefault()
-		this.removeTransition = false
+		if(this.draggingPayload.isDragging) return
 
+		this.removeTransition = false
 		const {min, max, step, styleWidth, getValue} = this.props
 		//此处用pageX,兼容有x轴滚动条的情况
 		const mouseLeft = e.pageX - this.offsetLeft
 		if( mouseLeft < 0 || mouseLeft > (styleWidth || 200) ) return
 		const percent = mouseLeft / (styleWidth || 200)
 		const value = Math.floor(percent * (max - min)) + min
+
 		//寻找最接近的handle的value值
-		const closestHandleValue = this.findClosestHandle(this.state, value)
+		const closestHandleValue = this.findClosestHandle(value)
 		const handleIndex = this.findHandleIndex(this.state, closestHandleValue )
 
 		const delta = value - closestHandleValue
@@ -151,7 +162,8 @@ export default class Slider extends Component{
 
 	validateDefault(defaultValue, min, max){
 		return defaultValue < min ? min : 
-												(defaultValue > max ?
+												(
+													defaultValue > max ?
 													max : defaultValue
 												)
 	}
@@ -160,8 +172,8 @@ export default class Slider extends Component{
 		return Object.prototype.toString.call(ele) === '[object Array]'
 	}
 
-	findClosestHandle(state, value){
-		let states = Object.keys(state)
+	findClosestHandle(value){
+		let states = Object.keys(this.state)
 		states = states.map((v) => {
 			return this.state[v]
 		})
@@ -182,63 +194,92 @@ export default class Slider extends Component{
 		return states.indexOf(v)
 	}
 
-	render(){
-		const {min, max, step, defaultValue, styleWidth, showTip, tipAlwayShow, customCls, theme, HandleAmount, defaultRange} = this.props
-		
-		const dv = this.isArray(defaultValue) ? defaultValue : [defaultValue]
-		const percentArr = [], styleObjArr = [] , handles = [], tipArr = []
+	//判断handle是否互相接触
+	isHandleMeet(index, value){
+		const {max, min} = this.props
+		let states = Object.keys(this.state)
+		states = states.map((v) => {
+			return this.state[v]
+		})
+		states.sort((a, b) => {
+			return a - b
+		})
 
+		const v = this.state[`currentValue${index}`]
+		const vIndex = states.indexOf(v)
+		const vMax = states[vIndex + 1] ? states[vIndex + 1] : max
+		const vMin = states[vIndex - 1] ? states[vIndex - 1] : min
+		return (value>vMin && value<vMax) ? false : true
+	}
+
+	render(){
+		const {min, max, step, defaultValue, styleWidth, isRound, isSolid, showTip, tipAlwayShow, 
+					customCls, theme, HandleAmount, isRange, rangeColors} = this.props
+		const dv = this.isArray(defaultValue) ? defaultValue : [defaultValue]
+		
 		const styleWidthObj = {
 			width : (styleWidth || 200) + 'px' 
 		}
 		const tipClass = classnames({
-			'ui-slider-tip' : true,
-			'ui-slider-tip-show' : tipAlwayShow
+			'ui-slider-tip' 			: true,
+			'ui-slider-tip-show'  : tipAlwayShow
 		})
 		const customClass = classnames({
-			'ui-slider-wraper' : true,
-			[customCls] : customCls,
-			[theme] : theme
+			'ui-slider-wraper' 	: true,
+			[customCls] 				: customCls,
+			'round' 						: ('round' in this.props || isRound),
+			'solid' 						: ('solid' in this.props || isSolid),
+			[`theme-${theme}`] 	: !!theme,
 		})
 
 		const transitionArray = ['WebkitTransition', 'MozTransition', 'msTransition', 'OTransition', 'Transition']
 
-		let i = 0
+		const rangeColorArray = rangeColors ? (this.isArray(rangeColors) ? rangeColors : [rangeColors]) : ['#fff', '#eee']
+
+		const handles = []
+		let i = 0, styleObjArr
 		while(i < HandleAmount){
 			let percent = (this.state[`currentValue${i}`] - min) / (max - min) * 100
-			percentArr.push( percent ) 
 
-			styleObjArr.push({
-				handle : { left: `${percent}%` },
-				back : { width: `${percent}%` },
-			})
+			styleObjArr = {
+				handle 	: { left: `${percent}%` },
+				back 		: { width: `${percent}%`,
+				 						backgroundColor : isRange && rangeColorArray[i],
+				 						zIndex :  10 - i
+				}
+			}
 			if(!this.removeTransition){
 				transitionArray.forEach(( transition ) => {
-					styleObjArr[i].handle[ transition ] = 'left 0.6s ease-out'
-					styleObjArr[i].back[ transition ] = 'width 0.6s ease-out'
+					styleObjArr.handle[ transition ] = 'left 0.6s ease-out'
+					styleObjArr.back[ transition ] = 'width 0.6s ease-out'
 				})
 			}
-			tipArr.push( (<em className={tipClass}>{this.state[`currentValue${i}`]}</em>) )
+			
 			handles.push(
-				<span key = {`handleKey${i}`} className='ui-slider-handle' style = { styleObjArr[i].handle }
-								onMouseDown = { !this.isTouchSuported && this.onHandleDown(i) }
-								onMouseMove = { !this.isTouchSuported && this.onHandleMove(i) } 
-								onMouseUp = { !this.isTouchSuported && this.onHandleUp(i) } 
-								onClick = {(e) => e.stopPropagation()}
-								onTouchStart = {this.isTouchSuported && this.onHandleDown(i)}
-								onTouchMove =  {this.isTouchSuported && this.onHandleMove(i)}
-								onTouchEnd = {this.isTouchSuported && this.onHandleMove(i)}>
-					{showTip && tipArr[i]}
-				</span>
+				<div key={`handleBack${i}`}>
+					<span 
+						key={`handle${i}`} 
+						className='ui-slider-handle' 
+						style={ styleObjArr.handle }
+						onMouseDown={ !this.isTouchSuported && this.onHandleDown(i) }
+						onMouseMove={ !this.isTouchSuported && this.onHandleMove(i) } 
+						onMouseUp={ !this.isTouchSuported && this.onHandleUp(i) } 
+						onClick={(e) => e.stopPropagation()}
+						onTouchStart={this.isTouchSuported && this.onHandleDown(i)}
+						onTouchMove= {this.isTouchSuported && this.onHandleMove(i)}
+						onTouchEnd={this.isTouchSuported && this.onHandleMove(i)}>
+						{showTip && (<em className={tipClass}>{this.state[`currentValue${i}`]}</em>)}
+					</span>
+					<div key={`Back${i}`} className='ui-slider-back' style = { styleObjArr.back }></div>
+				</div>
 			)
 			i++
 		}
 		
 		return(
 			<div className={customClass}>
-				<div className='ui-slider-horizontal' style = { styleWidthObj } ref='sliderBody' onClick = { this.clickhandler }>
+				<div className='ui-slider-horizontal' style={ styleWidthObj } ref='sliderBody' onClick={ this.clickhandler }>
 				{handles}
-				{!this.isArray(defaultValue) && <div className='ui-slider-back' style = { styleObjArr[0].back }></div>}
 				</div>
 			</div>
 		)
@@ -250,15 +291,25 @@ Slider.propTypes = {
 	max : PropTypes.number.isRequired,
 	step : PropTypes.number,
 	defaultValue : PropTypes.oneOfType([
-										PropTypes.number, 
-										PropTypes.array]),
+									PropTypes.number, 
+									PropTypes.array
+								]),
 	styleWidth: PropTypes.number,
 	getValue : PropTypes.func,
 	showTip : PropTypes.bool,
+	isRound : PropTypes.bool,
+	isSolid : PropTypes.bool,
+	isRange : PropTypes.bool,
+	isPass  : PropTypes.bool,
 	tipAlwayShow : PropTypes.bool,
 	customCls : PropTypes.string,
 	theme : PropTypes.string,
 	HandleAmount : PropTypes.number,
+	theme : PropTypes.oneOf(['default', 'info', 'success', 'warning', 'error']),
+	rangeColor : PropTypes.oneOfType([
+									PropTypes.string, 
+									PropTypes.array
+							]),
 }
 
 Slider.defaultProps = {
@@ -268,4 +319,8 @@ Slider.defaultProps = {
 	showTip : true,
 	tipAlwayShow: true,
 	HandleAmount : 1,
+	isRound : false,
+	isSolid : false,
+	isRange : false,
+	isPass  : true,
 }
--- examples/pages/SliderPage.jsx
+++ examples/pages/SliderPage.jsx
@@ -46,51 +46,96 @@ class SliderPage extends Component{
 
 		return(
 			<div style={{margin:'20px 20px 0 50px'}}>
-				<SliderContainer max = {200}/>
-				max = 200, 默认(min = 0, defaultValue = 0, step =1)
+				<SliderContainer max={200}/>
+				max=200, 默认(min=0, defaultValue=0, step =1)
 				
-				<SliderContainer min = {0} max = {300} step = {1} defaultValue = {80} />
-				min = 0, max = 300, step = 1, defaultValue = 80
+				<SliderContainer min={0} max={300} step={1} defaultValue={80} round={true}/>
+				min=0, max=300, step=1, defaultValue=80, round=true
 
-				<SliderContainer min = {40} max = {400} step = {5} defaultValue = {this.state.value1} 
-												getValue = {this.getValue1} showTip = {false}/>
+				<SliderContainer 
+					min={40} 
+					max={400} 
+					step={5} 
+					defaultValue={this.state.value1} 
+					getValue={this.getValue1} 
+					showTip={false}
+					solid={true}/>
 				<Button>{this.state.value1}</Button>
-				<div>配合其他component显示当前value: min = 40, max = 600, step = 5, defaultValue = 180, showTip = false</div>
+				<div>配合其他component显示当前value: min=40, max=600, step=5, defaultValue=180, showTip=false, solid=true</div>
 
-				<SliderContainer min = {60} max = {600} step = {1} defaultValue = {this.state.value2} 
-													getValue = {this.getValue2} styleWidth={800} tipAlwayShow = {false}/>
+				<SliderContainer 
+					min={60} 
+					max={600} 
+					step={1} 
+					defaultValue={this.state.value2} 
+					getValue={this.getValue2} 
+					styleWidth={800} 
+					tipAlwayShow={false}
+					round={true}/>
 				<Button>{this.state.value2}</Button>
-				<div>min = 60, max = 600, step = 1, defaultValue = 220, styleWidth = 800, tipAlwayShow = false</div>
+				<div>min=60, max=600, step=1, defaultValue=220, styleWidth=800, tipAlwayShow=false</div>
 
-				<SliderContainer min = {0} max = {10000} step = {500} defaultValue = {0} styleWidth={300}/>
-				<div>有较大step的slider： min = 0, max = 10000, step = 500, defaultValue = 0, styleWidth = 300</div>
+				<SliderContainer min={0} max={10000} step={500} defaultValue={0} styleWidth={300}/>
+				<div>有较大step的slider： min=0, max=10000, step=500, defaultValue=0, styleWidth=300</div>
 
-				<SliderContainer min = {-100} max = {300} step = {1} defaultValue = {-50} />
-				带负值的slider: min = -100, max = 300, step = 1, defaultValue = 80
+				<SliderContainer min={-100} max={300} step={1} defaultValue={-50} />
+				带负值的slider: min=-100, max=300, step=1, defaultValue=80
 
-				<SliderContainer min = {-100} max = {300} step = {1} defaultValue = {[50, 150]} HandleAmount = {2} getValue = {this.getValue3}/>
+				<SliderContainer min={-100} max={300} step={1} 
+					defaultValue={[50, 150]} 
+					HandleAmount={2} 
+					isRange={true}
+					getValue={this.getValue3}/>
 				<Button>{this.state.value3}</Button> <Button>{this.state.value4}</Button>
-				<div>选择范围: min = -100, max = 300, step = 1, defaultValue = [50, 150], HandleAmount = 2</div>
-				
-				<SliderContainer min = {-100} max = {800} step = {1} defaultValue = {[50, 150, 300, 300, 600, 1000]} 
-													HandleAmount = {7} styleWidth = {400} />
-				多点选择: min = -100, max = 800, step = 1, defaultValue = [50, 150, 300, 300, 600, 1000], HandleAmount = 7
+				<div>选择范围: min=-100, max=300, step=1, defaultValue=[50, 150], <b>isRange = true</b>, HandleAmount=2</div>
+				<div><b>范围选择器需设置isRange = true 才会显示中间段的颜色</b>, (better to set defaultValue in ascending order)</div>
+
+				<SliderContainer min={-20} max={100} step={1} 
+					defaultValue={[20, 60]} 
+					HandleAmount={2} 
+					isRange={true}
+					isPass={false}/>
+					<div>min=-20, max=100, step=1, defaultValue=[20, 60]</div>
+					<b>如需handle之间不越界，除了设置 isRange = true 之外还需设 isPass = false 保证handle之间不越界 </b>
+				<SliderContainer 
+					min={-100} 
+					max={800} step={1} 
+					defaultValue={[50, 150, 300, 350, 600, 700]} 
+					HandleAmount={7} 
+					styleWidth={400} 
+					isPass={false}/>
+				多点选择: min=-100, max=800, step=1, defaultValue=[50, 150, 300, 350, 600, 700], HandleAmount=7, isPass=false
 				<div>多点选择时handle的数量以HandleAmount为准. defaultValue的长度不足的默认为min,多余的将被忽略</div>
+				<div>better to set defaultValue in ascending order</div>
+
+				<SliderContainer 
+					min={-100} 
+					max={800} step={1} 
+					defaultValue={[50, 150, 300, 450, 600]} 
+					HandleAmount={5}
+					isRange={true} 
+					rangeColors={['blue', 'green', 'yellow', 'orange', 'red']}
+					styleWidth={400} />
+				多点选择: min=-100, max=800, step=1, defaultValue=[50, 150, 300, 300, 600, 700], HandleAmount=7
+				<div>isRange = true, rangeColors=['blue', 'green', 'yellow', 'orange', 'red']</div>
 				
-				<SliderContainer max = {200} defaultValue = {50} theme = {'slider-theme-green'}/>
-				内置的theme: max = 200, min = 0, defaultValue = 50, step =1, 
+				<SliderContainer max={200} defaultValue={50} theme={'default'} round={true}/>
+				theme default: max=200, min=0, defaultValue=50, step =1, round=true
 
-				<SliderContainer max = {200} defaultValue = {50} theme = {'slider-theme-red'}/>
-				内置的theme: max = 200, min = 0, defaultValue = 50, step =1, 
+				<SliderContainer max={200} defaultValue={50} theme={'info'} solid={true}/>
+				theme info: max=200, min=0, defaultValue=50, step =1, solid=true
 
-				<SliderContainer max = {200} defaultValue = {50} theme = {'slider-theme-yellow'}/>
-				内置的theme: max = 200, min = 0, defaultValue = 50, step =1, 
+				<SliderContainer max={200} defaultValue={50} theme={'success'} round={true}/>
+				theme success: max=200, min=0, defaultValue=50, step =1, round=true
 
-				<SliderContainer max = {200} defaultValue = {50} theme = {'slider-theme-blue'}/>
-				内置的theme: max = 200, min = 0, defaultValue = 50, step =1, 
+				<SliderContainer max={200} defaultValue={100} theme={'warning'} solid={true} round={true}/>
+				theme warning: max=200, min=0, defaultValue=100, step =1, solid=true, round=true
 
-				<SliderContainer min = {-300} max = {300} step = {1} defaultValue = {-100} customCls = 'mySlider'/>
-				自定义样式: min = -300, max = 300, step = 1, defaultValue = -100, customCls = 'mySlider'
+				<SliderContainer max={200} defaultValue={150} theme={'error'}/>
+				theme error: max=200, min=0, defaultValue=150, step =1, 
+
+				<SliderContainer min={-300} max={300} step={1} defaultValue={-100} customCls='mySlider'/>
+				自定义样式: min=-300, max=300, step=1, defaultValue=-100, customCls='mySlider'
 			</div>
 		)
 	}
@@ -100,7 +145,7 @@ const SliderContainer = ({
 	...params
 }) => {
 	return(
-		<div style = {{margin:'50px 0 10px'}}>
+		<div style={{margin:'50px 0 10px'}}>
 		 <Slider {...params}/>
 		</div>
 	)
