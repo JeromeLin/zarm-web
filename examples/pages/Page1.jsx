e5db24f59a1253b3ef18d7539b0a53ba6b19b858
Jerome Lin <jerome.lin@zhongan.com>
2016-06-24 00:47:56 +0800

    fix swipe transition bug

--- examples/pages/Page1.jsx
+++ examples/pages/Page1.jsx
@@ -141,6 +141,39 @@ class Page1 extends Component {
       <Loading visible={this.state.loading}>
         <div className="demo">
 
+          <Swipe>
+          <div className="ui-swipe-item">
+            <div className="ui-swipe-pic">
+              <a href="http://www.baidu.com">
+                <img src="http://map.baidu.com/fwmap/upload/h5v1.2-%E9%A6%96%E9%A1%B5banner-%E4%BD%B3%E4%B9%90%E9%94%AD.png" />
+              </a>
+            </div>
+            <div className="ui-swipe-info">
+              <div className="ui-swipe-title">百度</div>
+            </div>
+          </div>
+          <div className="ui-swipe-item">
+            <div className="ui-swipe-pic">
+              <a href="http://www.taobao.com">
+                <img src="http://map.baidu.com/fwmap/upload/h5v1.2-%E9%A6%96%E9%A1%B5banner-%E8%96%AF%E7%89%87-0.png" />
+              </a>
+            </div>
+            <div className="ui-swipe-info">
+              <div className="ui-swipe-title">淘宝</div>
+            </div>
+          </div>
+          <div className="ui-swipe-item">
+            <div className="ui-swipe-pic">
+              <a href="http://www.qq.com">
+                <img src="http://map.baidu.com/fwmap/upload/h5v1.2-%E9%A6%96%E9%A1%B5banner-%E9%BB%84%E6%B2%B9%E8%96%AF%E7%89%87-0.png" />
+              </a>
+            </div>
+            <div className="ui-swipe-info">
+              <div className="ui-swipe-title">腾讯</div>
+            </div>
+          </div>
+        </Swipe>
+        
           <span>Form</span>
           <Form style={{maxWidth: '100%'}}>
             <Form.Item label="账号">
