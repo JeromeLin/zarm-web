eba75af093c306b97b7e420569a1975e304dd542
王娜娜 <wangnana@zhongan.com>
2018-08-20 15:47:00 +0800

    fix: import Checkbox, Form, DataPicker

--- examples/pages/Tree.jsx
+++ examples/pages/Tree.jsx
@@ -1,7 +1,7 @@
 import Markdown from '../components/markdown';
+import Checkbox from '../../components/Checkbox'; // eslint-disable-line
 import '../../components/Mask/style';
 import '../../components/Button/style';
-
 
 export default class Mask extends Markdown {
   // eslint-disable-next-line
