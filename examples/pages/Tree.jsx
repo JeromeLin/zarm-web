22a1070f38c587515628a09de7f6d74df23537fc
王娜娜 <wangnana@zhongan.com>
2018-08-21 19:47:40 +0800

    feat: add Checkbox new props and update Tree component

--- examples/pages/Tree.jsx
+++ examples/pages/Tree.jsx
@@ -1,7 +1,7 @@
 import Markdown from '../components/markdown';
 import '../../components/checkbox/style';
 import '../../components/tree/style/index';
-import Checkbox from '../../components/checkbox';
+import Checkbox from '../../components/checkbox'; // eslint-disable-line
 
 export default class Mask extends Markdown {
   // eslint-disable-next-line
