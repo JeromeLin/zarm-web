d371669e340b39d28338d519eb9f10376a7a0f73
caianfa <caianfa@zhongan.com>
2019-01-15 16:40:34 +0800

    feat: select 新增远程搜索

--- components/select/Select.tsx
+++ components/select/Select.tsx
@@ -225,11 +225,12 @@ class Select extends Component<PropsType, StateProps> {
     this.props.onChange(selected, selectedData);
   }
 
-  onSearchValueChange = (e) => {
+  onSearchValueChange = (value) => {
     const { onSearchChange } = this.props;
+    // const textContent = (e.target as HTMLDivElement).textContent;
 
     this.setState({
-      searchValue: (e.target as HTMLDivElement).textContent,
+      searchValue: value,
       dropdown: true,
     }, () => {
       if (typeof onSearchChange === 'function') {
@@ -240,6 +241,7 @@ class Select extends Component<PropsType, StateProps> {
 
   render() {
     const { props } = this;
+    const { searchValue } = this.state;
     const {
       prefixCls,
       placeholder,
@@ -253,6 +255,7 @@ class Select extends Component<PropsType, StateProps> {
       multiple,
       getPopupContainer,
       locale,
+      remoteSearch,
     } = props;
 
     const disabled = 'disabled' in props || isDisabled;
@@ -282,12 +285,14 @@ class Select extends Component<PropsType, StateProps> {
 
     const { value } = this.state;
     const children: Array<ReactNode> = [];
-    this.state.optionData.forEach((elem, index) => {
-      if (search && this.state.searchValue) {
-        if (String(elem.props.children).indexOf(this.state.searchValue) === -1) {
-          return null;
-        }
+    const filterCondition = (option, optionIndex: number) => {
+      if (search && searchValue) {
+        return String(option.props.children).includes(searchValue);
+      } else { // remoteSearch会走此处逻辑
+        return optionIndex > -1;
       }
+    };
+    this.state.optionData.filter(filterCondition).forEach((elem, index) => {
       const checked = Array.isArray(value) ? value.indexOf(String(elem.value)) > -1 : String(elem.value) === value;
       children.push(
         <Option
@@ -339,6 +344,7 @@ class Select extends Component<PropsType, StateProps> {
           style={style}
           searchValue={this.state.searchValue}
           search={search}
+          remoteSearch={remoteSearch}
           active={this.state.dropdown}
           value={valueText}
           placeholder={placeholderText}
