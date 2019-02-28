a63dca82971bad774aa2a8b170721d1340e505bd
hzzheng <zhchaozju@gmail.com>
2019-02-28 17:23:36 +0800

    chore: fix some tslint warning

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -19,7 +19,7 @@ class Button extends Component<ButtonProps, any> {
     isLoading: false,
     className: null,
     style: {},
-    onClick: (e: MouseEvent) => { },
+    onClick: (_: MouseEvent) => {},
   };
 
   render() {
