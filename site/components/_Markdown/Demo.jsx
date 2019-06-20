eadcd429e2e81916d3cbccbf662bce653b1920ce
Jerome Lin <jerome.lin@zhongan.com>
2019-06-20 11:20:43 +0800

    chore: rename markdown

--- site/components/_Markdown/Demo.jsx
+++ site/components/_Markdown/Demo.jsx
@@ -1,30 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import marked from 'marked';
 import { transform } from '@babel/standalone';
-import { Icon } from 'zarm-web';
-import Editor from '../Editor';
-import '@/components/style/entry';
+import { Icon } from '@/components';
+import Editor from '../editor';
 
 export default class Canvas extends React.Component {
+  playerElem = null;
+
   constructor(props) {
-    console.log(props.children);
     super(props);
-    this.containerId = `${parseInt(Math.random() * 1e9, 10).toString(36)}`;
+
+    this.playerId = `${parseInt(Math.random() * 1e9, 10).toString(36)}`;
     this.document = props.children.match(/([^]*)\n?(```[^]+```)/);
+    this.description = marked(this.document[1]);
     this.source = this.document[2].match(/```(.*)\n?([^]+)```/);
+
     this.state = {
       showBlock: false,
     };
-    this.containerElem = null;
+    this.blockControl = this.blockControl.bind(this);
   }
+
 
   componentDidMount() {
     this.renderSource(this.source[2]);
   }
 
   componentWillUnmount() {
-    if (this.containerElem) {
-      ReactDOM.unmountComponentAtNode(this.containerElem);
+    if (this.playerElem) {
+      ReactDOM.unmountComponentAtNode(this.playerElem);
     }
   }
 
@@ -34,18 +39,22 @@ export default class Canvas extends React.Component {
   }
 
   renderSource(value) {
-    import('./node_modules/@/components').then((Element) => {
-      const args = ['context', 'React', 'ReactDOM', 'ZarmWeb'];
+    import('@/components').then((Element) => {
+      const args = ['context', 'React', 'ReactDOM', 'DragonUi'];
       const argv = [this, React, ReactDOM, Element];
-      return { args, argv };
-    }).then(({ args, argv }) => {
-      value = value
-        .replace(/import\s+\{\s+(.*)\s+\}\s+from\s+'zarm-web';/, 'const { $1 } = ZarmWeb;')
-        .replace('mountNode', `document.getElementById('${this.containerId}')`);
 
-      const { code } = transform(value, {
-        presets: ['es2015', 'react'],
-      });
+      return {
+        args,
+        argv,
+      };
+    }).then(({ args, argv }) => {
+      this.source[2] = value;
+
+      value = value
+        .replace(/import\s+\{\s+(.*)\s+\}\s+from\s+'dragon-ui';/, 'const { $1 } = DragonUi;')
+        .replace('mountNode', `document.getElementById('${this.playerId}')`);
+
+      const { code } = transform(value, { presets: ['es2015', 'react'] });
 
       args.push(code);
       // eslint-disable-next-line
@@ -58,19 +67,30 @@ export default class Canvas extends React.Component {
   }
 
   render() {
-    const { className } = this.props;
+    const { name } = this.props;
     const { showBlock } = this.state;
 
     return (
-      <div className={`demo-block demo-box ${className}`}>
+      <div className={`demo-block demo-box demo-${name}`}>
         <div
           className="source"
-          id={this.containerId}
-          ref={(elem) => { this.containerElem = elem; }}
+          id={this.playerId}
+          ref={(e) => {
+            this.playerElem = e;
+          }}
         />
         {
           showBlock && (
             <div className="meta">
+              {
+                this.description && (
+                  <div
+                    className="description"
+                    // eslint-disable-next-line
+                    dangerouslySetInnerHTML={{ __html: this.description }}
+                  />
+                )
+              }
               <Editor
                 value={this.source[2]}
                 onChange={code => this.renderSource(code)}
@@ -78,7 +98,7 @@ export default class Canvas extends React.Component {
             </div>
           )
         }
-        <div className="demo-block-control" onClick={() => this.blockControl()}>
+        <div className="demo-block-control" onClick={this.blockControl}>
           {
             showBlock ? (
               <span>
