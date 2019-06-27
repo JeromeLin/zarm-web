4fad937b4c5ee48e9257dd06f92d7db4faa19952
Jerome Lin <jerome.lin@zhongan.com>
2019-06-27 16:28:46 +0800

    style: update

--- components/button/ButtonGroup.tsx
+++ components/button/ButtonGroup.tsx
@@ -5,13 +5,9 @@ import { ButtonGroupProps } from './PropsType';
 const ButtonGroup: React.SFC<ButtonGroupProps> = (props) => {
   const { prefixCls = 'zw-btn-group', size, className, ...others } = props;
 
-  const cls = classNames(
-    prefixCls,
-    {
-      [`${prefixCls}--${size}`]: size,
-    },
-    className,
-  );
+  const cls = classNames(prefixCls, className, {
+    [`${prefixCls}--${size}`]: size,
+  });
 
   return <div {...others} className={cls} />;
 };
