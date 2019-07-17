c25bac62b21cbc2b272040ddf12dbc2cef6b2a57
caianfa <caianfa@zhongan.com>
2019-07-17 17:55:21 +0800

    fix: add some md page not convert code

--- site/components/Markdown/index.jsx
+++ site/components/Markdown/index.jsx
@@ -4,6 +4,8 @@ import marked from 'marked';
 import Demo from './Demo';
 import './style.scss';
 
+const withOutConvertPage = ['ChangeLog', 'QuickStart'];
+
 export default class Markdown extends React.Component {
   constructor(props) {
     super(props);
@@ -41,41 +43,42 @@ export default class Markdown extends React.Component {
   }
 
   render() {
-    const { document } = this.props;
+    const { document, name } = this.props;
+    const renderer = new marked.Renderer();
 
     if (typeof document === 'string') {
-      this.components.clear();
+      if (!withOutConvertPage.includes(name)) {
+        this.components.clear();
 
-      const renderer = new marked.Renderer();
+        renderer.table = (header, body) => {
+          return `<table class="grid" id="grid"><thead>${header}</thead><tbody>${body}</tbody></table>`;
+        };
 
-      renderer.table = (header, body) => {
-        return `<table class="grid" id="grid"><thead>${header}</thead><tbody>${body}</tbody></table>`;
-      };
-
-      let html = marked(
-        document
-        // .replace(/## API\s?([^]+)/g, '')
-        //   .replace(/##\s*API\s?([^]+)/g, '$1')
+        let html = marked(
+          document
+          // .replace(/## API\s?([^]+)/g, '')
+          //   .replace(/##\s*API\s?([^]+)/g, '$1')
           // .replace(/(```\s?jsx([^]+?)```)/g, (match, p1) => {
-          .replace(/##\s?([^]+?)((?=##))/g, (match, p1) => {
-            const id = parseInt(Math.random() * 1e9, 10).toString(36);
-            // console.log(p1)
-            this.components.set(id, React.createElement(Demo, { ...this.props }, p1));
-            return `<div id=${id} class="markdown-demo-item"></div>`;
-          }),
-        {
-          renderer,
-        },
-      );
+            .replace(/##\s?([^]+?)((?=##))/g, (match, p1) => {
+              const id = parseInt(Math.random() * 1e9, 10).toString(36);
+              // console.log(p1)
+              this.components.set(id, React.createElement(Demo, { ...this.props }, p1));
+              return `<div id=${id} class="markdown-demo-item"></div>`;
+            }),
+          {
+            renderer,
+          },
+        );
 
-      html = html.replace('##', '').replace('API', '<h2 id="api-node" style="margin-top: 50px">API</h2>');
+        html = html.replace('##', '').replace('API', '<h2 id="api-node" style="margin-top: 50px">API</h2>');
+        // eslint-disable-next-line react/no-danger
+        return <div dangerouslySetInnerHTML={{ __html: html }} className="markdown-outer" ref={(el) => { this.markdownCon = el; }} />;
+      }
 
-      return (
-        // eslint-disable-next-line
-        <>
-          <div dangerouslySetInnerHTML={{ __html: html }} className="markdown-outer" ref={(el) => { this.markdownCon = el; }} />
-        </>
-      );
+      const html = marked(document, { renderer });
+
+      // eslint-disable-next-line react/no-danger
+      return <div dangerouslySetInnerHTML={{ __html: html }} className="markdown-outer" ref={(el) => { this.markdownCon = el; }} />;
     }
 
     return <span />;
