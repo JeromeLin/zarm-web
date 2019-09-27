be379785a8488859fc7d007519e41f30056ce73c
王娜娜 <wangnana@zhongan.com>
2019-09-27 14:42:18 +0800

    chore: update Avatar propsType

--- components/avatar/PropsType.tsx
+++ components/avatar/PropsType.tsx
@@ -2,11 +2,12 @@ export interface PropsType {
   prefixCls?: string;
   icon?: string;
   shape?: 'circle' | 'square';
-  size?: number | 'lg' | 'sm';
+  size?: number | 'lg' | 'sm' | 'xl' | 'xs';
   src?: string;
   alt?: string;
   className?: string;
 }
+
 export interface StateType {
   loadError: boolean;
 }
