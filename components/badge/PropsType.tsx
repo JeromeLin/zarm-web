af8dff7cfa2c9c4f73b4b220e69349fc078275e7
王娜娜 <wangnana@zhongan.com>
2019-09-27 10:51:36 +0800

    chore: update badge propsType

--- components/badge/PropsType.tsx
+++ components/badge/PropsType.tsx
@@ -1,7 +1,3 @@
-export default interface PropsType {
-  theme?: 'default' | 'primary' | 'success' | 'warning' | 'danger';
-  shape?: 'dot' | 'radius' | 'round' |'rect'| 'circle' | 'leaf';
-  sup?: boolean;
-  text?: React.ReactNode;
-  style?: React.CSSProperties;
-}
+import { BadgeProps } from 'zarm/types/badge';
+
+export default BadgeProps;
