9186c4514de5edcc0f0cb5954fd482d2cf932b3a
Jerome <jerome.lin@zhongan.com>
2019-10-17 19:24:30 +0800

    chore: update propstype

--- components/badge/index.tsx
+++ components/badge/index.tsx
@@ -1,6 +1,5 @@
-import Badge, { BadgeProps } from 'zarm/lib/badge';
+import Badge from 'zarm/lib/badge';
 
 Badge.defaultProps.prefixCls = 'zw-badge';
 
-export { BadgeProps };
 export default Badge;
