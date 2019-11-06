820c3b6da600a0744e8ff598c2b786895b83a822
Jerome Lin <jerome.lin@zhongan.com>
2019-11-06 16:53:26 +0800

    chore: remove useless component

--- components/index.tsx
+++ components/index.tsx
@@ -16,7 +16,6 @@ export { default as Popover } from './popover';
 export { default as Progress } from './progress';
 export { default as Radio } from './radio';
 export { default as Slider } from './slider';
-export { default as Step } from './step';
 export { default as Steps } from './steps';
 export { default as Swipe } from './swipe';
 export { default as Switch } from './switch';
