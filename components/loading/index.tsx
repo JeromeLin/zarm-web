c6e57ece889ec6eb6f2deffd5ec29d2ef20f84f2
develop-zw <wb_zhangwen@zhongan.io>
2019-11-14 18:04:29 +0800

    fix: loading opacity

--- components/loading/index.tsx
+++ components/loading/index.tsx
@@ -133,21 +133,28 @@ class Loading extends Component<LoadingProps, LoadingStates> {
     );
 
     if (this.isNestedPattern() || fullscreen) {
+      const containerCls = classnames(`${prefixCls}__container`, {
+        [`${prefixCls}--blur`]: !!visible,
+      });
       return (
         <div className={cls} style={this.getStyle()}>
           {!!visible && (
-            <div className={`${prefixCls}__mask`}>
-              <div className={`${prefixCls}__spin`} style={style}>
-                {this.renderIndicator()}
-                {text && (
-                  <div className={`${prefixCls}__text`} style={textCls}>
-                    {text}
-                  </div>
-                )}
+            <div>
+              <div className={`${prefixCls}__mask`}>
+                <div className={`${prefixCls}__spin`} style={style}>
+                  {this.renderIndicator()}
+                  {text && (
+                    <div className={`${prefixCls}__text`} style={textCls}>
+                      {text}
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
           )}
-          {children}
+          <div className={containerCls}>
+            {children}
+          </div>
         </div>
       );
     }
