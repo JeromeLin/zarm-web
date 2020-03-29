8863194eb1e643032434ea05375336dc646930fa
elvinzhu <yongyu168@foxmail.com>
2020-03-29 17:17:15 +0800

    refactor: drop the title element when there is no title

--- components/notification/Notification.tsx
+++ components/notification/Notification.tsx
@@ -49,9 +49,7 @@ export class Notification extends React.Component<NotificationProps, {}> {
       >
         <div className={cls} onClick={onClick}>
           {iconToRender}
-          {title && <div className={`${prefixCls}__head`}>
-            {title}
-          </div>}
+          {title && <div className={`${prefixCls}__head`}>{title}</div>}
           {content && <div className={`${prefixCls}__body`}>{content}</div>}
           {footer && <div className={`${prefixCls}__foot`}>{footer}</div>}
         </div>
